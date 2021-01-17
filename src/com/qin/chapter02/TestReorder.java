98b7a61f9888fab8776d231bb5c2c3ecfaf813d3
qgs <3210968849@qq.com>
2021-01-17 22:45:27 +0800

    测试重排序

---
+++ src/com/qin/chapter02/TestReorder.java
@@ -0,0 +1,40 @@
+package com.qin.chapter02;
+
+public class TestReorder {
+
+    public static int num = 0;
+    public static boolean ready = false;
+
+    public static class ReadThread extends Thread {
+        @Override
+        public void run() {
+            while (!Thread.currentThread().isInterrupted()) {
+                if (ready) {
+                    System.out.println(num + num);
+                }
+                System.out.println("read thread...");
+            }
+        }
+    }
+
+    public static class WriteThread extends Thread {
+        @Override
+        public void run() {
+            num = 2;
+            ready = true;
+            System.out.println("write thread set over...");
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ReadThread read = new ReadThread();
+        read.start();
+
+        WriteThread write = new WriteThread();
+        write.start();
+
+        Thread.sleep(10);
+        read.interrupt();
+        System.out.println("main exit");
+    }
+}
